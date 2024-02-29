db9e585f4a5f5960f2b20abb99991a721979d09e
Luiz Cabral <luizfelipe.cabralsouza@gmail.com>
2024-02-29 11:09:32 -0300

    Refatorando código

--- teste_estagio.js
+++ teste_estagio.js
@@ -1,37 +1,37 @@
 //para este código foi utilizado operador ternário
 let alimento = {
 	nome_generico:
-		document.getElementById("field_generic_name_value") == null
+		document.querySelector("#field_generic_name_value") == undefined
 			? "Nome do alimento indisponível"
-			: document.getElementById("field_generic_name_value").textContent,
+			: document.querySelector("#field_generic_name_value").textContent,
 	marca:
-		document.getElementById("field_brands_value") == null
+		document.querySelector("#field_brands_value") == null
 			? "Marca do alimento indisponível"
-			: document.getElementById("field_brands_value").textContent,
+			: document.querySelector("#field_brands_value").textContent,
 	ingredientes:
 		document
-			.getElementById("panel_ingredients_content")
-			.getElementsByClassName("panel_text")[0] == null
+			.querySelector("#panel_ingredients_content")
+			.querySelector(".panel_text") == null
 			? "Ingredientes do alimento indisponíveis"
 			: document
-					.getElementById("panel_ingredients_content")
-					.getElementsByClassName("panel_text")[0]
+					.querySelector("#panel_ingredients_content")
+					.querySelector(".panel_text")
 					.textContent.trim(),
 	nutri_score: document
-		.getElementById("attributes_grid")
-		.getElementsByTagName("h4")[0]
+		.querySelectorAll(".attr_text")[0]
+		.querySelector("h4")
 		.textContent.split(" ")
 		.slice(1)
 		.join(" "),
 	nova: document
-		.getElementById("attributes_grid")
-		.getElementsByTagName("h4")[1]
+		.querySelectorAll(".attr_text")[1]
+		.querySelector("h4")
 		.textContent.split(" ")
 		.slice(1)
 		.join(" "),
 	eco_score: document
-		.getElementById("attributes_grid")
-		.getElementsByTagName("h4")[2]
+		.querySelectorAll(".attr_text")[2]
+		.querySelector("h4")
 		.textContent.split(" ")
 		.slice(1)
 		.join(" "),
